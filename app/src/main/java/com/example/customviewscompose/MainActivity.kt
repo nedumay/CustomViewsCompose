d78eb4366452c79e68b5f1b39507f8bf77fc0349
Samim <Samim058@gamil.com>
2024-08-05 13:08:49 +0300

    Draw line

--- app/src/main/java/com/example/customviewscompose/MainActivity.kt
+++ app/src/main/java/com/example/customviewscompose/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.customviewscompose.samples.CanvasTest
 import com.example.customviewscompose.ui.theme.CustomViewsComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CustomViewsComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            CanvasTest()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CustomViewsComposeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/customviewscompose/samples/CanvasTest.kt
@@ -0,0 +1,74 @@
+package com.example.customviewscompose.samples
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun CanvasTest() {
+    var ponints by rememberSaveable {
+        mutableStateOf<List<Point>>(listOf())
+    }
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragStart = {
+                        ponints = ponints + Point(it, isStartedPosition = true)
+                    },
+                    onDrag = { change, _ ->
+                        ponints = ponints + change.historical.map {
+                            Point(offset = it.position, isStartedPosition = false)
+                        }
+                    }
+                )
+            }
+    ) {
+        val path = Path()
+        ponints.forEach{
+            if(it.isStartedPosition){
+                path.moveTo(it.offset.x, it.offset.y)
+            }else{
+                path.lineTo(it.offset.x, it.offset.y)
+            }
+        }
+        drawPath(
+            path = path,
+            brush = Brush.linearGradient(listOf(Color.Red, Color.Blue)),
+            style = Stroke(10.dp.toPx())
+        )
+    }
+}
+
+data class Point(
+    val offset: Offset,
+    val isStartedPosition: Boolean
+)
+
+@Composable
+fun Dp.toPx() = with(LocalDensity.current) {
+    this@toPx.toPx()
+}
